4f08de6396954d1a0541280fccc0a088c981d562
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-13 13:26:04 -0800

    initial work

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    if (size > 0) {
+      var temp = storage[0];
+      debugger;
+      _.each(storage, function(item, key){
+        var newKey = parseInt(key)--;
+        if(newKey > -1){
+          storage[newKey] = item;
+        }
+      });
+
+      size--;
+
+      return temp;
+    }
+
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    if (size > 0) {
+      size--;
+    }
+    return storage[size];
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
