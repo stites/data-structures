baf320daa280932bd23e25585e7b27113284400d
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-13 16:06:59 -0800

    Implemented the functional-shared queue function

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,41 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  instance.storage = {};
+  instance.length = 0;
+
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+
+  dequeue: function(){
+    if (this.length) {
+      this.length--;
+      var temp = this.storage[0];
+      for(var key in this.storage){
+        var newKey = parseInt(key) - 1;
+        if (newKey > -1) {
+          this.storage[newKey]=this.storage[key];
+        }
+      }
+      delete this.storage[this.length];
+
+      return temp;
+    }
+
+  },
+
+  size: function(){
+    return this.length;
+  }
+
+};
