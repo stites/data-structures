56605353e94cb8eee121a4acb07bbef98be88421
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-13 16:23:20 -0800

    Implimented Pseudoclassical datastructure

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,19 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+	this.storage = {};
+	this.length = 0;
+};
+
+
+Stack.prototype.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function(){
+  this.length && this.length--;
+  return this.storage[this.length];
+};
+
+Stack.prototype.size = function(){
+  return this.length;
 };
