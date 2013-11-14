63ad75b5f16c581dd4e9bb0b21887301e8d3e8e0
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-13 16:15:06 -0800

    Implemented prototypal queue data structure

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,36 @@
 var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+
+  instance.storage = {};
+  instance.length = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+
+  dequeue: function(){
+    if (this.length) {
+      this.length--;
+      var temp = this.storage[0];
+      for(var key in this.storage){
+        var newKey = parseInt(key) - 1;
+        if (newKey > -1) {
+          this.storage[newKey]=this.storage[key];
+        }
+      }
+      delete this.storage[this.length];
+
+      return temp;
+    }
+  },
+
+  size: function(){
+    return this.length;
+  }
+
+};
