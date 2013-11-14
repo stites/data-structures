227b0343514df5c353ca26790f84ca985b933f82
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-13 16:29:38 -0800

    Pseudoclassical implementation of queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,29 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.length = 0;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.length) {
+    this.length--;
+    var temp = this.storage[0];
+    for(var key in this.storage){
+      var newKey = parseInt(key) - 1;
+      if (newKey > -1) {
+        this.storage[newKey]=this.storage[key];
+      }
+    }
+    delete this.storage[this.length];
+
+    return temp;
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.length;
 };
