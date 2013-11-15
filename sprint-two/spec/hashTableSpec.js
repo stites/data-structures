88d738fbbd63bd09013d83cb719e13016caf18fe
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-14 20:07:43 -0800

    should insert multiple elements into the HashTable

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -17,6 +17,14 @@ describe("hashTable", function() {
     expect(hashTable._storage.get(hash)).toEqual(v1);
   });
 
+  it("should insert multiple elements into the HashTable", function(){
+    var v1 = 'val1', v2 = 'val2';
+    hashTable.insert(v1, v1);
+    hashTable.insert(v2, v2);
+    expect(hashTable.retrieve(v1)).toEqual(v1);
+    expect(hashTable.retrieve(v2)).toEqual(v2);
+  });
+
   xit("should handle hash function collisions", function(){
     // force the hash function to return 0
     spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
