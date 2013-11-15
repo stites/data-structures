ca11c6c314581317e383b9c319bc788ff5854d00
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-15 15:20:07 -0800

    Created basic files for doublyLinkedList

---
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -0,0 +1,145 @@
+describe("doublyLinkedList", function() {
+  var linkedList;
+
+  beforeEach(function() {
+    linkedList = makeLinkedList();
+  });
+
+  it("should have a head and tail", function() {
+    expect(Object.keys(linkedList)).toContain("head");
+    expect(Object.keys(linkedList)).toContain("tail");
+  });
+
+  it("should have methods named 'addToTail', 'removeHead', and 'contains'", function() {
+    expect(linkedList.addToTail).toEqual(jasmine.any(Function));
+    expect(linkedList.removeHead).toEqual(jasmine.any(Function));
+    expect(linkedList.contains).toEqual(jasmine.any(Function));
+  });
+
+  it("should have a null head and tail when first instantiated", function() {
+    expect(linkedList.head).toEqual(null);
+    expect(linkedList.tail).toEqual(null);
+  });
+
+  describe('.addToTail()', function () {
+    it('should make head and tail point to the same object when run once on an empty list', function(){
+      linkedList.addToTail(4);
+      expect(linkedList.tail).toEqual(jasmine.any(Object));
+      expect(linkedList.head).toEqual(linkedList.tail);
+    });
+
+    it('head and tail do not point to the same object if run more than once', function(){
+      linkedList.addToTail(4);
+      linkedList.addToTail(8);
+      expect(linkedList.head).toNotEqual(linkedList.tail);
+    });
+
+    it('the first node has a .next property which points to tail when addToTail is run twice', function(){
+      linkedList.addToTail(4);
+      linkedList.addToTail(8);
+      expect(linkedList.head.next).toEqual(linkedList.tail);
+    });
+
+    it('the second-to-last node has a .next property which points to tail when addToTail is run more than once', function(){
+      linkedList.addToTail(4);
+      linkedList.addToTail(8);
+      expect(linkedList.head.next).toEqual(linkedList.tail);
+      linkedList.addToTail(12);
+      expect(linkedList.head.next.next).toEqual(linkedList.tail);
+      linkedList.addToTail(16);
+      expect(linkedList.head.next.next.next).toEqual(linkedList.tail);
+    });
+  });
+
+  describe('.removeHead()', function () {
+    it('.removeHead() method moves head and tail to `null` if no items are left in list', function(){
+      linkedList.addToTail(4);
+      linkedList.removeHead();
+      expect(linkedList.head).toEqual(null);
+      expect(linkedList.tail).toEqual(null);
+    });
+
+    it('should change the head to null from a multi-node list', function(){
+      linkedList.addToTail(4);
+      linkedList.addToTail(8);
+      linkedList.addToTail(12);
+      linkedList.removeHead();
+      expect(linkedList.head.value).toEqual(8);
+    });
+
+    it('should moves the head pointer to the next node in the list', function(){
+      linkedList.addToTail(4);
+      var firstAdd = linkedList.head;
+      linkedList.addToTail(4);
+      linkedList.addToTail(4);
+      linkedList.removeHead();
+      expect(linkedList.head).toNotEqual(firstAdd);
+      var secondAdd = linkedList.head;
+      linkedList.removeHead();
+      expect(linkedList.head).toNotEqual(secondAdd);
+    });
+
+    it('.removeHead() method returns the value of the node which was removed', function(){
+      linkedList.addToTail(9);
+      linkedList.addToTail(8);
+      linkedList.addToTail(7);
+      var expected0 = linkedList.head.value;
+      var head0 = linkedList.removeHead();
+      expect(head0).toEqual(expected0);
+      var expected1 = linkedList.head.value;
+      var head1 = linkedList.removeHead();
+      expect(head1).toEqual(expected1);
+    });
+  });
+
+  describe('.contains()',function () {
+    it('should returns a boolean reflecting whether or not the passed-in value is in a single-valued list', function(){
+      linkedList.addToTail(9);
+      expect(linkedList.contains(9)).toEqual(true);
+      expect(linkedList.contains(11)).toEqual(false);
+    });
+
+    it('should returns a boolean reflecting whether or not the passed-in value is in a multi-valued list', function(){
+      linkedList.addToTail(9);
+      linkedList.addToTail(8);
+      linkedList.addToTail(7);
+      linkedList.addToTail(6);
+      linkedList.addToTail(5);
+      linkedList.addToTail(4);
+      linkedList.addToTail(3);
+      linkedList.addToTail(2);
+      linkedList.addToTail(1);
+      linkedList.addToTail(0);
+      expect(linkedList.contains(5)).toEqual(true);
+      expect(linkedList.contains(11)).toEqual(false);
+    });
+  })
+
+  describe('.addToHead()', function () {
+    it('should make head and tail point to the same object when run once on an empty list', function(){
+      linkedList.addToHead(4);
+      expect(linkedList.tail).toEqual(jasmine.any(Object));
+      expect(linkedList.head).toEqual(linkedList.tail);
+    });
+
+    it('head and tail do not point to the same object if run more than once', function(){
+      linkedList.addToHead(4);
+      linkedList.addToHead(8);
+      expect(linkedList.head).toNotEqual(linkedList.tail);
+    });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,54 @@
+// Note: don't use an array to do this.
+var makeLinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    var newNode = makeNode(value);
+
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = list.head;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+  };
+
+  list.removeHead = function(){
+    var newHead = list.head.next;
+    var value = list.head.value;
+    list.head = null;
+
+    if (newHead === null){
+      list.tail = null;
+    } else {
+      list.head = newHead;
+    }
+
+    return value;
+  };
+
+  list.contains = function(value){
+    var node = list.head;
+    do {
+      if (node.value === value){
+        return true;
+      }
+      (node.next) && (node = node.next);
+    } while (node.next !== null)
+
+    return false;
+  };
+
+  return list;
+};
+
+var makeNode = function(value){
+  var node = {};
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
