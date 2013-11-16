9bf950e9780239264610402a152bbcfa81b2e0f2
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-16 09:53:05 -0800

    should have methods named 'insert', 'contains', and 'depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,20 @@
 var makeBinarySearchTree = function(){
+  var binarySearchTree = Object.create(baseTree);
+
+  return binarySearchTree;
 };
+
+var baseTree = makeTree();
+
+baseTree.insert = function () {
+
+};
+
+baseTree.contains = function () {
+
+};
+
+baseTree.depthFirstLog = function () {
+
+};
+
