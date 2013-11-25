47abb5e14dcdefe2241a8ace9a213ba332f918f7
Sam Stites <stites.sr@gmail.com>
2013-11-24 23:52:47 -0800

    refactor for add

--- sprint-two/src/bloomFilter.js
+++ sprint-two/src/bloomFilter.js
@@ -8,13 +8,9 @@ BloomFilter.prototype.getHashes = function() {
 };
 
 BloomFilter.prototype.add = function(strVal) {
-  var results = [];
-  for (var i = 0; i < this._hashStorage.length; i++) {
-    results.push(this._hashStorage[i](strVal, this._limit));
-  };
   var mask = 0;
-  for (var i = 0; i < results.length; i++) {
-    mask |= (1 << results[i]);
+  for (var i = 0; i < this._hashStorage.length; i++) {
+    mask |= 1 << this._hashStorage[i](strVal, this._limit);
   };
   this._storage |= mask;
 };
