9cec5f9c33d1c5bbf47e34956328f66ffe6f3ded
Sam Stites <stites.sr@gmail.com>
2013-11-29 18:01:29 -0800

    should convert a single string into a tree with string.length nodes +1 for root

--- sprint-two/spec/prefixTreeSpec.js
+++ sprint-two/spec/prefixTreeSpec.js
@@ -8,11 +8,28 @@ describe('prefixTree', function () {
       expect(prefixTree instanceof Tree).toBeTruthy();
       expect(prefixTree instanceof PrefixTree).toBeTruthy();
     });
+    it('should not have a root value', function() {
+      expect(prefixTree.value).toEqual(undefined);
+    });
   });
 
-  describe('batch uploading of strings', function () {
+  describe('uploading a string', function () {
+    it('should have a function called "stringUpload."', function () {
+      expect(prefixTree.stringUpload).toEqual(jasmine.any(Function))
+    });
+    it('should convert a single string into a tree with string.length nodes +1 for root', function () {
+      prefixTree.stringUpload('this');
+      var counter = 0;
+      prefixTree.traverse(function () {
+        counter++;
+      });
+      expect(counter).toEqual('this'.length+1);
+    });
+  });
+
+  xdescribe('batch uploading of words', function () {
     it('should have a function called "batchUpload."', function () {
       expect(prefixTree.batchUpload).toEqual(jasmine.any(Function))
-    })
+    });
   })
 });
--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -5,6 +5,12 @@ var PrefixTree = function PrefixTree(value, parent) {
 PrefixTree.prototype = Object.create(Tree.prototype);
 PrefixTree.prototype.constructor = PrefixTree;
 
-PrefixTree.prototype.batchUpload = function(csv) {
+PrefixTree.prototype.stringUpload = function(str) {
+  var charsToAdd = str.split('');
+  for (var i = 0; i < charsToAdd.length; i++) {
+    this.addChild(charsToAdd[i]);
+  };
+};
 
+PrefixTree.prototype.batchUpload = function() {
 };
